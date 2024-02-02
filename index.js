113e0acaab670d93d07da1b98b04a64e4b120cf7
Kenneth <kvmoore097@gmail.com>
2024-02-01 20:54:39 -0600

    phase-1 algorithms palindrome

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+
+// Rewrite the Problem in Your Own Words
+// Write Your Own Test Cases
+// Pseudocode
+// Code
+// Make It Clean and Readable
+// Optimize
+
+// Write a function isPalindrome that will receive one argument, a string. Your function should return true 
+// if the string is a palindrome (that is, if it reads the same forwards and backwards, like "mom" or "racecar"), 
+// and return false if it is not a palindrome.
+
+// To keep things simple, your function only needs to deal with lowercase strings that are all letters (don't worry about spaces or special characters).
+
+    function isPalindrome(string) {
+      
+      const cleanedString = string.replace(/[^a-z]/g, '').toLowerCase();
+   
+      return cleanedString === cleanedString.split('').reverse().join('');
+   }
+   
+   console.log(isPalindrome('race car'));
+
+//Test cases:
+
+console.log('Expecting to return true')
+console.log(isPalindrome('race car'))
 
 /* 
-  Add your pseudocode here
+Add your pseudocode here
+
+Create a function that takes in a string
+    only needs to deal with lowercase strings that are letters.
+    Don't worry about spaces or special characters
+
+if string is a palindrome it should return true and should return false is if it's not
+
 */
 
 /*
   Add written explanation of your solution here
+
+Function takes in a string and removes spaces special characters and converts it to lower case.
+It then takes the original string and compares it to the reverse of the string using a split, reverse and join
+
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
